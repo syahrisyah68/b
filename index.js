7a27f6cffa804db2181efbd16662801811120dd4
nsrstory <76632542+syahrisyah68@users.noreply.github.com>
2021-11-10 20:29:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -187,7 +187,7 @@ autoketik = false
 autoregister = setting.user_register
 
 img = setting.img
-baper = 'SUB YT ZEEONE OFC'
+baper = 'SYAHRI-BOT'
 apiku = 'https://youtu.be/2rWbjrDIAyA'
 gc_wa_lu = 'https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV' //klo gk punya gc wa gk usah di ganti 👍
 targetpc = setting.ownerNumberr
